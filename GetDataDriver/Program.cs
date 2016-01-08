104e8c5adbd7af6ee94df1e2e1fdece70a794f7f
Tommy Durant <thomas.durant@yale.edu>
2016-01-08 16:53:55 -0500

    Don't load data into memory, keep data and averaging on the elastic node.

--- GetDataDriver/Program.cs
+++ GetDataDriver/Program.cs
@@ -18,27 +18,61 @@ namespace GetDataDriver
             var settings = new ConnectionSettings(node, index);
             var elasticClient = new ElasticClient(settings);
 
-            var queryPath = @"C:\Users\thoma\Documents\00GitHub\MovingAverages\JsonQueries\glucoseAggregationAverage.txt";
+            var aggPath = @"C:\Users\thoma\Documents\00GitHub\MovingAverages\JsonQueries\glucoseAggregationAverage.txt";
+            var filterPath = @"C:\Users\thoma\Documents\00GitHub\MovingAverages\JsonQueries\glucoseRange.txt";
 
-
-            System.IO.StreamReader myFile = new System.IO.StreamReader(queryPath);
+            System.IO.StreamReader myFile = new System.IO.StreamReader(aggPath);
             string myString = myFile.ReadToEnd();
 
-
+            System.IO.StreamReader filterFile = new System.IO.StreamReader(filterPath);
+            string filterString = filterFile.ReadToEnd();
 
             var result = elasticClient.Search<Result>(s => s
                 .From(0)
                 .QueryRaw(myString)
                 );
 
-            var name = (string)null;
+            var glucoseFilter = elasticClient.Search<Result>(s => s
+                .From(0)
+                .Size(20000)
+                .QueryRaw(filterString)
+                .Sort(o => o.OnField(p => p.Timestamp).Ascending())
+                );
 
-            var myAgg = result.Aggs.Average("glucose").Value;
+            foreach(var hit in glucoseFilter.Hits)
+            {
+                //2015-10-02T08:58:00
+                Console.WriteLine(hit.Source.Timestamp);
+                DateTime date = hit.Source.Timestamp;
+            }
+            
 
-            var glucoseAvg = myAgg;
+           
+            var totalDocumentCount = glucoseFilter;
+            var glucoseAvg = result.Aggs.Average("glucose").Value;
 
+            
 
-            Console.WriteLine(name);
+            //for (var i = totalDocumentCount; i – windowSize > 0 && averages.Count() <= 25; i = i – step)
+            //{
+
+            //    var endIdx = i;
+
+            //    var startIdx = i – windowSize;
+
+            //    // can include the {} part in your text file, 
+            //    // then read it into a variable with the string replacement operator. It should then replace
+            //    // the {variableName} piece with what’s defined above
+
+            //    var query = $”some json here where we order by descending result_timestamp and take documents { startIdx}
+            //    through { endIdx}”;
+
+            //    var avg = query.Average(r => r.Value);
+
+            //    averages.Add(avg);
+
+            //}
+
             myFile.Close();
         }
     }
