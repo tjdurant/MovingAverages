6c4e7bcbe0dd72e4499f3777a17a87734e36e607
Tommy Durant <thomas.durant@yale.edu>
2016-01-12 14:54:41 -0500

    extra code taken out; going to add string replacement to more easily change interval and window size, etc.

--- GetDataDriver/Program.cs
+++ GetDataDriver/Program.cs
@@ -36,66 +36,26 @@ namespace GetDataDriver
                 .QueryRaw(myString)
                 );
 
-            var glucoseFilter = elasticClient.Search<Result>(s => s
-                .From(0)
-                .QueryRaw(filterString)
-                .Sort(o => o.OnField(p => p.Timestamp).Ascending())
-                );
-
             var agBucket = (Bucket)result.Aggregations["my_date_histo"];
 
 
-            using (var sw = new StreamWriter(@"C:\Users\thoma\Documents\00GitHub\MovingAverages\dataTest.csv"))
+
+            var movingAverageList = new List<KeyValuePair<DateTime, double?>>();
+            foreach (HistogramItem item in agBucket.Items)
             {
-                var writer = new CsvWriter(sw);
-
-                var movingAverageList = new List<KeyValuePair<DateTime, double?>>();
-                foreach (HistogramItem item in agBucket.Items)
-                {
-                    var mov_avg = (ValueMetric)item.Aggregations["glucose_avg"];
-                    var date = item.Date;
+                var mov_avg = (ValueMetric)item.Aggregations["glucose_avg"];
+                var date = item.Date;
                     
-                    var avg_value = mov_avg.Value;
-                    //Write entire current record
-                    movingAverageList.Add(new KeyValuePair<DateTime, double?>(date, avg_value));
-                    //writer.WriteRecord(num);
+                var avg_value = mov_avg.Value;
 
-                    //var top1 = topHits.Hits<PlacementVerificationES>().Single();
-                    //var reportingObject = RepoToReporting(top1);
-                    //output.Add(reportingObject);
+                movingAverageList.Add(new KeyValuePair<DateTime, double?>(date, avg_value));
                     
-                }
-
-                foreach (var element in movingAverageList)
-                {
-                    Console.WriteLine(element);
-                }
-
-                sw.Close();
             }
 
-
-            //for (var i = totalDocumentCount; i – windowSize > 0 && averages.Count() <= 25; i = i – step)
-            //{
-
-            //    var endIdx = i;
-
-            //    var startIdx = i – windowSize;
-
-            //    // can include the {} part in your text file, 
-            //    // then read it into a variable with the string replacement operator. It should then replace
-            //    // the {variableName} piece with what’s defined above
-
-            //    var query = $”some json here where we order by descending result_timestamp and take documents { startIdx}
-            //    through { endIdx}”;
-
-            //    var avg = query.Average(r => r.Value);
-
-            //    averages.Add(avg);
-
-            //}
-
-            myFile.Close();
+            foreach (var element in movingAverageList)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
